2d9903862e37fdae24e5a15595108692b6b89be6
Abolfazlmollashahi <abolfazlmollashahi0937@gmail.com>
2024-11-13 14:19:03 +0330

    tst2

--- Front-end/src/main.jsx
+++ Front-end/src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import {store} from './redux/store.js'
+import { Provider } from 'react-redux'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store} >
   <BrowserRouter>
     <App />
   </BrowserRouter>
+  </Provider>
 );
--- Front-end/src/route.jsx
+++ Front-end/src/route.jsx
@@ -1,10 +1,35 @@
+import Cart from "./pages/Cart";
+import CategorySearch from "./pages/CategorySearch";
+import Dashbord from "./pages/Dashbord";
 import Home from "./pages/Home";
+import LoginAndSinup from "./pages/LoginAndSinup";
+import ShowProduct from "./pages/Product/ShowProduct";
 
 
 const routes = [
   {
     path:'/',
     element:<Home/>
-  }
+  },
+  {
+    path:'/category',
+    element:<CategorySearch/>
+  },
+  {
+    path:'/cart',
+    element:<Cart/>
+  },
+  {
+    path:'/login',
+    element:<LoginAndSinup />
+  },
+  {
+    path:'/dashbord',
+    element:<Dashbord/>
+  },
+  {
+    path:'/showproduct/:id',
+    element:<ShowProduct/>
+  },
 ]
 export default routes
