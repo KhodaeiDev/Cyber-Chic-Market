80aeeb84ef9b67c1cff2e4cd3429e597b38ce438
khodaeidev <khodaei.dev@gmail.com>
2024-12-06 19:01:00 +0330

    User routes anbd files changed to profile routes in proFile modules

--- Back-end/modules/profile/profile.controller.js
+++ Back-end/modules/profile/profile.controller.js
@@ -1,21 +1,26 @@
 const userModel = require("./../../models/User");
+const orderModel = require("./../../models/order");
 const bcrypt = require("bcrypt");
 const {
   editValidator,
   resetPasswordValidator,
 } = require("./profile.validator");
 
+const { errorResponse, successResponse } = require("../../helpers/responses");
+const cities = require("./../../cities/cities.json");
+
 exports.getUserInfo = async (req, res, next) => {
   try {
-    const userID = req.user._id;
+    const user = req.user;
 
-    const user = await userModel.findOne({ _id: userID });
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
+    const orders = await orderModel.find({ user: user._id });
 
     user.password = undefined;
-    return res.status(200).json({ user });
+
+    return successResponse(res, 200, {
+      user,
+      orders,
+    });
   } catch (err) {
     next(err);
   }
@@ -78,6 +83,84 @@ exports.updatePassword = async (req, res, next) => {
     );
 
     return res.json({ message: "Password Updated successfully" });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.createAddress = async (req, res, next) => {
+  try {
+    const user = req.user;
+    const { name, postalCode, address, location, cityId, provinceId } =
+      req.body;
+
+    const city = cities.find((city) => +city.id === +cityId);
+    const province = cities.find((province) => +province.id === +provinceId);
+
+    if (!city || !province) {
+      return errorResponse(res, 409, "Province Or City is Not Valid");
+    }
+
+    const addressObject = {
+      name,
+      postalCode,
+      location,
+      cityId,
+      address,
+      provinceId,
+    };
+
+    const updateAddress = await userModel.findOneAndUpdate(
+      user._id,
+      {
+        $push: { addresses: addressObject },
+      },
+      { new: true }
+    );
+
+    updateAddress.password = undefined;
+    return successResponse(res, 200, {
+      updateAddress,
+      message: "Address Added Successfully",
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getAllUserAdresses = async (req, res, next) => {
+  try {
+    const user = req.user;
+
+    const addreses = user.addresses;
+
+    user.password = undefined;
+    return successResponse(res, 200, {
+      addreses,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.removeAddress = async (req, res, next) => {
+  try {
+    const { addressId } = req.params;
+    const user = await userModel.findOne({ _id: req.user.id });
+
+    const address = user.addresses.id(addressId);
+    if (!address) {
+      return errorResponse(res, 404, "Address Not Found!!");
+    }
+
+    await user.addresses.pull(address);
+    await user.save();
+    user.password = undefined;
+
+    return successResponse(res, 200, {
+      user,
+      message: "Address Removed Successfully",
+    });
   } catch (err) {
     next(err);
   }
