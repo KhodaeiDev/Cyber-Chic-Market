96678edb785363545ffbfa67724506ce442faecc
khodaeidev <khodaei.dev@gmail.com>
2024-12-06 18:59:47 +0330

    Edit in files of project

--- Back-end/app.js
+++ Back-end/app.js
@@ -6,7 +6,6 @@ const path = require("path");
 const authRouter = require("./modules/auth/auth.routes");
 const categoryRouter = require("./modules/category/category.routes");
 const locationsRouter = require("./modules/locations/locations.routes.js");
-const userRouter = require("./modules/user/user.routes");
 const productRouter = require("./modules/product/product.routes");
 const commentsRouter = require("./modules/comments/comment.routes.js");
 const cartRouter = require("./modules/cart/cart.routes.js");
@@ -33,7 +32,6 @@ app.use("/images", express.static(path.join(__dirname, "public/images")));
 //* Routes
 app.use("/auth", authRouter);
 app.use("/locations", locationsRouter);
-app.use("/users", userRouter);
 app.use("/category", categoryRouter);
 app.use("/products", productRouter);
 app.use("/comments", commentsRouter);
