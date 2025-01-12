482e450156a6d54c06410754a253b52dd87e414e
Abolfazlmollashahi <abolfazlmollashahi0937@gmail.com>
2025-01-12 13:22:39 +0330

    fix

--- Front-end/tailwind.config.js
+++ Front-end/tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -15,7 +17,7 @@ export default {
     },
   },
   plugins: [
-    require('daisyui'),
+    daisyui
   ],
 }
 
