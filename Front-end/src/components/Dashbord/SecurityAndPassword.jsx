22c6562eb5247e27e15254ef4a22dd1b3e5b1091
Abolfazlmollashahi <abolfazlmollashahi0937@gmail.com>
2025-01-18 01:33:35 +0330

    fix SecurityAndPassword

--- Front-end/src/components/Dashbord/SecurityAndPassword.jsx
+++ Front-end/src/components/Dashbord/SecurityAndPassword.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 function SecurityAndPassword() {
   return  (
     <div className=" w-full flex flex-col items-center justify-center min-h-max">
-      <div className="w-full max-w-lg p-8 bg-white rounded-lg shadow-lg">
-        <h2 className="text-xl font-semibold text-gray-700 mb-4 border-b-2 border-teal-500 pb-2">بازنشانی کلمه عبور</h2>
+      <div className="w-full max-w-lg p-8 bg-white rounded-lg shadow-md">
+        <h2 className="text-xl font-semibold text-gray-700 mb-4 border-b-2 border-col pb-2">بازنشانی کلمه عبور</h2>
         
-        <div className="space-y-6">
+        <div className="space-y-6 flex flex-col justify-center">
           {/* New Password */}
           <div>
             <label className="block text-gray-600 mb-1">* کلمه عبور جدید</label>
@@ -28,12 +28,12 @@ function SecurityAndPassword() {
           </div>
           
           {/* Reset Password Button */}
-          <button className="w-full py-2 bg-teal-500 text-white rounded-full hover:bg-teal-600 focus:outline-none">
+          <button className="w-[50%] mx-auto py-2 bg-btn  rounded-full">
             بازنشانی کلمه عبور
           </button>
           
           {/* Password Security Message */}
-          <div className="p-4 mt-4 text-sm text-gray-600 bg-teal-50 border-l-4 border-teal-300 rounded">
+          <div className="p-4 mt-4 text-sm text-gray-600 bg-teal-50 border-l-4 border-col rounded">
             رمز عبور خود را محافظت کرده و از افشای آن به دیگران خودداری کنید، همچنین از استفاده از رمزهای ضعیف و قابل پیش‌بینی جلوگیری کنید.
           </div>
         </div>
